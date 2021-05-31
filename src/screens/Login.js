7f827b871bef1b7f2fbbdfb86a2e705a4e6a74d1
Max16 <max16@naver.com>
2021-05-31 17:33:54 +0900

    #8.7 Introduction to Styled Components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,38 @@
 import { isLoggedInVar } from "../apollo";
+import styled, { css } from "styled-components";
+import { useState } from "react";
+
+const Title = styled.h1`
+  color: ${(props) => (props.potato ? "violet" : "beige")};
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  ${(props) =>
+    props.potato
+      ? css`
+          font-size: 100px;
+        `
+      : css`
+          text-decoration: underline;
+        `}
+`;
+
+const Container = styled.div`
+  background-color: tomato;
+`;
+
+const TogglePotato = styled.button`
+  color: red;
+`;
 
 const Login = () => {
+  const [potato, setPotato] = useState(false);
+  const togglePotato = () => setPotato((current) => !current);
   return (
-    <div>
-      <h1>Login</h1>
+    <Container>
+      <Title potato={potato}>Login</Title>
       <button onClick={() => isLoggedInVar(true)}>Log in</button>
-    </div>
+      <TogglePotato onClick={togglePotato}>Toggle Potato</TogglePotato>
+    </Container>
   );
 };
 
