14bab585584464764644d060be046b0640d3124c
Max16 <max16@naver.com>
2021-05-31 17:43:29 +0900

    #8.8 Themes on Styled Components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,37 +1,21 @@
-import { isLoggedInVar } from "../apollo";
-import styled, { css } from "styled-components";
-import { useState } from "react";
+import { darkModeVar, isLoggedInVar } from "../apollo";
+import styled from "styled-components";
 
 const Title = styled.h1`
-  color: ${(props) => (props.potato ? "violet" : "beige")};
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  ${(props) =>
-    props.potato
-      ? css`
-          font-size: 100px;
-        `
-      : css`
-          text-decoration: underline;
-        `}
+  color: ${(props) => props.theme.fontColor};
 `;
 
 const Container = styled.div`
-  background-color: tomato;
-`;
-
-const TogglePotato = styled.button`
-  color: red;
+  background-color: ${(props) => props.theme.bgColor};
 `;
 
 const Login = () => {
-  const [potato, setPotato] = useState(false);
-  const togglePotato = () => setPotato((current) => !current);
   return (
     <Container>
-      <Title potato={potato}>Login</Title>
+      <Title>Login</Title>
+      <button onClick={() => darkModeVar(true)}>To Dark</button>
+      <button onClick={() => darkModeVar(false)}>To light</button>
       <button onClick={() => isLoggedInVar(true)}>Log in</button>
-      <TogglePotato onClick={togglePotato}>Toggle Potato</TogglePotato>
     </Container>
   );
 };
