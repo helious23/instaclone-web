09eabe66cd870a076899c6f122765f17d8c7ad77
Max16 <max16@naver.com>
2021-06-04 17:47:37 +0900

    #10.14 Dark Mode

--- src/styles.js
+++ src/styles.js
@@ -3,11 +3,16 @@ import reset from "styled-reset";
 
 export const lightTheme = {
   accent: "#0095f6",
+  bgColor: "#FAFAFA",
   borderColor: "rgb(219, 219, 219)",
+  fontColor: "rgb(38,38,38)",
+  formBgColor: "#fafafa",
 };
 export const darkTheme = {
+  accent: "gray",
   fontColor: "lightgray",
-  bgColor: "#2c2c2c",
+  bgColor: "#000",
+  formBgColor: "black",
 };
 
 export const GlobalStyles = createGlobalStyle`
@@ -19,10 +24,10 @@ export const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
     }
     body{
-        background-color: #FAFAFA;
+        background-color: ${(props) => props.theme.bgColor};
         font-size: 14px;
         font-family: 'Open Sans', sans-serif;
-        color: rgb(38,38,38);
+        color: ${(props) => props.theme.fontColor};
     }
     a{
       text-decoration: none;
