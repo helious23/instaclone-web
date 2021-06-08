de29b2d5d7e061971fc0fa74f6208e8bf90c8401
Max16 <max16@naver.com>
2021-06-08 16:01:13 +0900

    11.18 Create Comment part Three

--- src/components/feed/Comments.js
+++ src/components/feed/Comments.js
@@ -3,7 +3,18 @@ import gql from "graphql-tag";
 import PropTypes from "prop-types";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import useUser from "../../hooks/useUser";
 import Comment from "./Comment";
+
+const CREATE_COMMENT_MUTATION = gql`
+  mutation createComment($photoId: Int!, $payload: String!) {
+    createComment(photoId: $photoId, payload: $payload) {
+      ok
+      error
+      id
+    }
+  }
+`;
 
 const CommentContainer = styled.div`
   margin-top: 20px;
@@ -30,20 +41,62 @@ const PostCommentInput = styled.input`
   }
 `;
 
-const CREATE_COMMENT_MUTATION = gql`
-  mutation createComment($photoId: Int!, $payload: String!) {
-    createComment(photoId: $photoId, payload: $payload) {
-      ok
-      error
-    }
-  }
-`;
-
 const Comments = ({ photoId, author, caption, commentNumber, comments }) => {
+  const { data: userData } = useUser();
+  const { register, handleSubmit, setValue, getValues } = useForm();
+  const createCommentUpdate = (cache, result) => {
+    const { payload } = getValues();
+    setValue("payload", "");
+    const {
+      data: {
+        createComment: { ok, id },
+      },
+    } = result;
+    if (ok && userData?.me) {
+      const newComment = {
+        __typename: "Comment",
+        createAt: Date.now() + "",
+        id,
+        isMine: true,
+        payload,
+        user: {
+          ...userData.me,
+        },
+      };
+      const newCacheComment = cache.writeFragment({
+        data: newComment,
+        fragment: gql`
+          fragment SomeName on Comment {
+            id
+            createdAt
+            isMine
+            payload
+            user {
+              username
+              avatar
+            }
+          }
+        `,
+      });
+      cache.modify({
+        id: `Photo:${photoId}`,
+        fields: {
+          comments(prev) {
+            return [...prev, newCacheComment];
+          },
+          commentNumber(prev) {
+            return prev + 1;
+          },
+        },
+      });
+    }
+  };
   const [createCommentMutation, { loading }] = useMutation(
-    CREATE_COMMENT_MUTATION
+    CREATE_COMMENT_MUTATION,
+    {
+      update: createCommentUpdate,
+    }
   );
-  const { register, handleSubmit, setValue } = useForm();
   const onValid = (data) => {
     const { payload } = data;
     if (loading) {
@@ -55,7 +108,6 @@ const Comments = ({ photoId, author, caption, commentNumber, comments }) => {
         payload,
       },
     });
-    setValue("payload", "");
   };
   return (
     <CommentContainer>
